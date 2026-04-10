a2a69601260f212e367ef65e002139834f1f712c
Mohit kumar <mohitkumargpj0@gmail.com>
2026-04-10 19:34:05 +0530

    Update connection string to MongoDB Atlas and secure with env variables

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/bookmart';
+// const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/bookmart';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://MohitKumar:<db_password>@cluster0.buzokvu.mongodb.net/?appName=Cluster';
 
 mongoose.connect(MONGO_URI)
   .then(() => {
